204b0cf44982b5466b9f92106e6c40b18243c4af
Diego Stratta <strattadb@gmail.com>
2018-09-10 00:21:26 -0300

    chore: generate changelog with release

--- release.config.js
+++ release.config.js
@@ -1,10 +1,25 @@
+const changelogFile = 'CHANGELOG.md';
+const changelogTitle = 'Changelog';
+
+const gitPluginAssets = ['package.json', 'yarn.lock', changelogFile];
+
+const changelogPlugin = {
+  path: '@semantic-release/changelog',
+  changelogFile,
+  changelogTitle,
+};
+
+const npmPlugin = '@semantic-release/npm';
+
+const gitPlugin = {
+  path: '@semantic-release/git',
+  assets: gitPluginAssets,
+};
+
+const githubPlugin = '@semantic-release/github';
+
 module.exports = {
   branch: 'master',
-  prepare: [
-    '@semantic-release/npm',
-    {
-      path: '@semantic-release/git',
-      assets: ['package.json', 'yarn.lock', 'CHANGELOG.md'],
-    },
-  ],
+  verifyConditions: [changelogPlugin, npmPlugin, gitPlugin, githubPlugin],
+  prepare: [changelogPlugin, npmPlugin, gitPlugin],
 };
